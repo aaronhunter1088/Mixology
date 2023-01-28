507f6b1e69cf5dded8c3381f5cd287433457b244
Michael Ball <aaronhunter@live.com>
2023-01-28 15:46:25 -0600

    adding glasstype

---
+++ src/main/groovy/GlassType.groovy
@@ -0,0 +1,18 @@
+import lombok.ToString
+
+@ToString
+enum GlassType {
+
+    SHOT("Shot glass"),
+    OLDFASHIONED("Old Fashioned glass"),
+    HIGHBALL("Highball glass"),
+    COCKTAIL("Cocktail glass"),
+    TOMCOLLINS("Tom Collins glass"),
+    WINE("Wine glass"),
+    HURRICANE("Hurricane glass")
+
+    String glassName
+    GlassType(glassName) {
+        this.glassName = glassName
+    }
+}
